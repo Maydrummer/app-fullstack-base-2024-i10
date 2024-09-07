6cbab8edc0aea96d26bdf57665ac6279e0ef33ef
Anthony <thonymmj21@gmail.com>
2024-09-07 12:23:10 -0500

    clase 3, POO

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -1,3 +1,8 @@
+class Persona
+{
+    public nombre:string;
+    public edad:number;
+}
 
 function SayHello(){
     let current_value = document.getElementById("textarea_1") as HTMLInputElement;
@@ -6,6 +11,15 @@ function SayHello(){
 
 }
 
+function mostrar(per:Persona)
+{
+    alert(per.nombre + " " + per.edad);
+}
+
 window.addEventListener("load",  ()=> {
     SayHello();
+    let persona1:Persona = new Persona();
+    persona1.edad = 26;
+    persona1.nombre = "Anthony Maisincho";
+    mostrar(persona1);
 });
