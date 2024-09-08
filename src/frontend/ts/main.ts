b14bb742fe52daa8938ec3351220061fe16d8a8f
Anthony <thonymmj21@gmail.com>
2024-09-08 12:03:03 -0500

    Clase 3 culminada, se agrego class main para encapsular boton y agregar evento, se crea user.ts para agregar otra clase

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -1,13 +1,24 @@
 class Main implements EventListenerObject{
-    private mensaje:string = "Hello world";
+    public mensaje:string;
+    public usuario:Usuario;
+    constructor(mensaje:string, usuario:Usuario)
+    {
+        this.mensaje = mensaje;
+        this.usuario = usuario;
+        let btn = this.recuperarElemento("btn");
+        btn.addEventListener('click',this);
+    }
     handleEvent(object: Event): void {
-        alert(this.mensaje);
+        let alerta:string = `El usuario:${this.usuario.usuario} que tiene ${this.usuario.edad} anios dice: ${this.mensaje}`;
+        alert(alerta);
+    }
+    private recuperarElemento(id:string):HTMLInputElement
+    {
+        return <HTMLInputElement> document.getElementById(id); 
     }
 }
 
 window.addEventListener("load", () => {
-    let btn=document.getElementById('btn')
-    let objeto_main = new Main();
-    btn.addEventListener('click',objeto_main);
-
+    let user1 = new Usuario("Maydrummer",27);
+    let objeto_main = new Main("NO PAIN NO GAIN",user1);
 });
---
+++ src/frontend/ts/user.ts
@@ -0,0 +1,9 @@
+class Usuario{
+    public usuario:string;
+    public edad:number;
+    constructor(usuario:string,edad:number)
+    {
+        this.usuario = usuario;
+        this.edad = edad;
+    }
+}
