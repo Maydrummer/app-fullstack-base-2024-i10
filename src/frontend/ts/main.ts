0183b4086649637450c142c19a269714ff3dacca
Anthony Maisincho <thonymmj21@gmail.com>
2024-10-06 11:14:48 -0500

    Se consulto al endpoint de dispositvos y se muestra en interfaz web, falta poder agregar nuevos dispositivos (clase 4)

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -5,17 +5,55 @@ class Main implements EventListenerObject{
     {
         this.mensaje = mensaje;
         this.usuario = usuario;
-        let btn = this.recuperarElemento("btn");
-        btn.addEventListener('click',this);
+        let btn_recargar = this.recuperarElemento("btn_devices");
+        btn_recargar.addEventListener('click',this);
+        let btn_agregar = this.recuperarElemento("btn_matricular");
+        btn_agregar.addEventListener('click',this);
     }
     handleEvent(object: Event): void {
-        let alerta:string = `El usuario:${this.usuario.usuario} que tiene ${this.usuario.edad} anios dice: ${this.mensaje}`;
-        alert(alerta);
+        let id_actual = (object.target as HTMLElement).id;
+        if(id_actual == "btn_devices")
+        {
+            console.log("boton para enlistar dispositivos");
+            this.obtener_dispositivos();
+
+        }
+        else if (id_actual == "btn_matricular")
+        {
+            console.log("Boton para matricular equipos");
+        }
+
     }
     private recuperarElemento(id:string):HTMLInputElement
     {
         return <HTMLInputElement> document.getElementById(id); 
     }
+
+    private obtener_dispositivos():void
+    {
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    console.log(xhr.responseText);
+                    let str_to_show : string='' ;
+                    let div_refresh = this.recuperarElemento("list");
+                    let lista: Array<Device> = JSON.parse(xhr.responseText);
+                    for(let element of lista)
+                    {
+                        str_to_show += "<h4>"+element.name + "</h4> - " + element.description + " prendido=" + element.state;
+                        str_to_show += "<br>"
+
+                    }
+                    div_refresh.innerHTML = str_to_show;
+
+                }
+            }
+        }
+        xhr.open("GET","http://localhost:8000/devices/", true);
+        xhr.send();
+    }
+
 }
 
 window.addEventListener("load", () => {
